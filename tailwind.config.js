aed92b0e8b6cd30868ea4750f51cc7c1748a44a3
Randy Oest <oest@amazingrando.com>
2023-08-17 11:18:28 -0400

    cursor text

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
       cursor: {
         arrow: 'url(/cursor-arrow.svg), auto',
         hand: 'url(/cursor-hand.svg), pointer',
-        text: 'url(/cursor-text.svg), text',
+        text: 'text',
       },
       fontFamily: {
         base: ['Aleo', 'serif'],
