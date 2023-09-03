8d8d70a2eb8a5512d19bdc5f4bf2a394335d2a3d
Randy Oest <oest@amazingrando.com>
2023-09-03 14:32:39 -0400

    metadata

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -2,7 +2,8 @@ const metadataDefaults = {
   title: 'The Content Strategy Quiz • Better Results Through Content',
   description:
     'Are your content strategy practices holding you back? Identify gaps, strengths, and opportunities for success with our quiz.',
-  image: '/sharing.png',
+  image: 'https://content-maturity-quiz.netlify.app/sharing.png',
+  url: 'https://content-maturity-quiz.netlify.app/',
 };
 
 export default metadataDefaults;
