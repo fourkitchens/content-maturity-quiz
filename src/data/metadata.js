6194abb2b391d7b31e9b6faf556012dcd5ad0fa6
Randy Oest <oest@amazingrando.com>
2023-09-03 13:55:03 -0400

    metadata

---
+++ src/data/metadata.js
@@ -0,0 +1,8 @@
+const metadataDefaults = {
+  title: 'The Content Strategy Quiz • Better Results Through Content',
+  description:
+    'Are your content strategy practices holding you back? Identify gaps, strengths, and opportunities for success with our quiz.',
+  image: '/sharing.png',
+};
+
+export default metadataDefaults;
